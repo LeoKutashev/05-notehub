a6a83bb900f5f6290680377c9807edc81c9e5a34
Leonid <leo.kutashev@gmail.com>
2025-10-21 23:12:35 +0200

    Update

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,41 +1,38 @@
-
 import css from './NoteList.module.css';
-import type { Note } from '../../types/note';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteNote } from '../../services/noteService';
+import type { Note } from '../../types/note';
 
-interface NoteListProps {
-  notes: Note[];
-}
-
-export default function NoteList({ notes }: NoteListProps) {
+export default function NoteList({ notes }: { notes: Note[] }) {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
-    mutationFn: (id: number) => deleteNote(id),
+    mutationFn: deleteNote,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['notes'] });
     },
   });
 
-  const handleDelete = (id: number) => {
-    mutation.mutate(id);
-  };
-
-    return (
-        <ul className={css.list}>
-            {notes.map(note => {
-                return (
-                    <li key={note.id} className={css.listItem}>
-                        <h2 className={css.title}>{note.title}</h2>
-                        <p className={css.content}>{note.content}</p>
-                        <div className={css.footer}>
-                            <span className={css.tag}>{note.tag}</span>
-                            <button className={css.button} onClick={()=>mutation.mutate(note.id)}>Delete</button>
-                        </div>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+  return (
+    <ul className={css.list}>
+      {notes.map(note => {
+        return (
+          <li key={note.id} className={css.listItem}>
+            <h2 className={css.title}>{note.title}</h2>
+            <p className={css.content}>{note.content}</p>
+            <div className={css.footer}>
+              <span className={css.tag}>{note.tag}</span>
+              <button 
+                className={css.button} 
+                onClick={() => mutation.mutate(note.id.toString())}
+                disabled={mutation.isPending}
+              >
+                {mutation.isPending ? 'Deleting...' : 'Delete'}
+              </button>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
--- src/types/note.ts
+++ src/types/note.ts
@@ -1,6 +1,10 @@
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+
 export interface Note {
-    id: number;
-    title: string;
-    content: string;
-    tag: string;
+  id: number;
+  title: string;
+  content: string;
+  tag: NoteTag;
+  createdAt: string;  
+  updatedAt: string;  
 }
